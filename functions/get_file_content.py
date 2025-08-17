94f3cd847dc5064d6f89b9573eee451e210b1926
smurphy11 <ownerofallisee@gmail.com>
2025-08-16 22:03:59 -0400

    All finished up

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 from config import MAX_CHARS
+from google.genai import types
 
 
 def get_file_content(working_directory, file_path):
@@ -19,3 +20,17 @@ def get_file_content(working_directory, file_path):
             return file_content_string
     except Exception as e:
         return f'Error: Something went wrong: {e}'
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Read file contents from the file in the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to read from, relative to the working directory.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from call_function import call_function, available_functions
+from prompts import system_prompt
+from config import MAX_ITERS
 
 
 
 
 def main():
     print("Hello from aiagent2!")
+    
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -29,18 +33,60 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=prompt)]),
         ]
+    
+    iters = 0
+    while True:
+        iters += 1
+        if iters > MAX_ITERS:
+            print(f"Maximum iterations ({MAX_ITERS}) reached.")
+            sys.exit(1)
 
-   
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+
+def generate_content(client, messages, verbose):   
     response = client.models.generate_content(
         model="gemini-2.0-flash-001", 
-        contents= messages)
+        contents= messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction = system_prompt)
+        )
+    
     if verbose:
-        print(f"User prompt: {prompt}")
-    print(response.text)
-    if verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
 
+
+    if response.candidates:
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
+    if not response.function_calls:
+        return response.text
+
+    function_responses = []
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
+        ):
+            raise Exception("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
+
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+
+    messages.append(types.Content(role="user", parts=function_responses))
 
 if __name__ == "__main__":
     main()
